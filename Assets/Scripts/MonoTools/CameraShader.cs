624cef3026cb9387986584d61d6c5d6ac1ea75aa
包涵 <1004578954@qq.com>
2020-07-06 13:27:01 +0800

    Trafficlight Camera

---
+++ Assets/Scripts/MonoTools/CameraShader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShader : MonoBehaviour
+{
+    public Material material;
+    void Awake()
+    {
+        material = new Material(Shader.Find("Hidden/UpsideDownImageEffectShader"));
+    }
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, material);
+    }
+}
